09c6cf97aafcaff00819077c248d2216be0d355a
Onat Turkcuoglu <dogan.tuerkcueoglu@student.hpi.uni-potsdam.de>
2014-10-16 20:46:47 +0200

    let there be light, viva luminos

---
+++ scripts/luminos.lua
@@ -0,0 +1,164 @@
+function luminosProject(LUA_DIR)
+
+        LUMINOS_DIR = BGFX_DIR .. "luminos/"
+        _name = "luminos"
+	project (_name)
+		uuid (os.uuid(_name))
+		kind "WindowedApp"
+
+	configuration {}
+
+	debugdir (BGFX_DIR .. "examples/runtime/")
+
+	includedirs {
+		BX_DIR .. "include",
+		BGFX_DIR .. "include",
+		BGFX_DIR .. "3rdparty",
+		BGFX_DIR .. "examples/common",
+                LUA_DIR .. "include",
+		"$(SDL2_DIR)/include",
+	}
+
+	files {
+	        LUMINOS_DIR .. "src/**.cpp",
+	        LUMINOS_DIR .. "src/**.h",
+	}
+
+	links {
+		"bgfx",
+		"example-common",
+                "lua51",
+	}
+        
+        libdirs { LUA_DIR .. "/src" }
+
+	if _OPTIONS["with-sdl"] then
+		defines { "ENTRY_CONFIG_USE_SDL=1" }
+		links   { "SDL2" }
+
+		configuration { "x32", "windows" }
+			libdirs { "$(SDL2_DIR)/lib/x86" }
+
+		configuration { "x64", "windows" }
+			libdirs { "$(SDL2_DIR)/lib/x64" }
+
+		configuration {}
+	end
+
+	configuration { "vs*" }
+		linkoptions {
+			"/ignore:4199", -- LNK4199: /DELAYLOAD:*.dll ignored; no imports found from *.dll
+		}
+		links { -- this is needed only for testing with GLES2/3 on Windows with VS2008
+			"DelayImp",
+		}
+
+	configuration { "vs201*" }
+		linkoptions { -- this is needed only for testing with GLES2/3 on Windows with VS201x
+			"/DELAYLOAD:\"libEGL.dll\"",
+			"/DELAYLOAD:\"libGLESv2.dll\"",
+		}
+
+	configuration { "windows" }
+		links {
+			"gdi32",
+			"psapi",
+		}
+
+	configuration { "android*" }
+		kind "ConsoleApp"
+		targetextension ".so"
+		linkoptions {
+			"-shared",
+		}
+		links {
+			"EGL",
+			"GLESv2",
+		}
+
+	configuration { "nacl or nacl-arm" }
+		kind "ConsoleApp"
+		targetextension ".nexe"
+		links {
+			"ppapi",
+			"ppapi_gles2",
+			"pthread",
+		}
+
+	configuration { "pnacl" }
+		kind "ConsoleApp"
+		targetextension ".pexe"
+		links {
+			"ppapi",
+			"ppapi_gles2",
+			"pthread",
+		}
+
+	configuration { "asmjs" }
+		kind "ConsoleApp"
+		targetextension ".bc"
+
+	configuration { "linux-*" }
+		links {
+			"X11",
+			"GL",
+			"pthread",
+		}
+
+	configuration { "rpi" }
+		links {
+			"X11",
+			"GLESv2",
+			"EGL",
+			"bcm_host",
+			"vcos",
+			"vchiq_arm",
+			"pthread",
+		}
+
+	configuration { "osx" }
+		files {
+			BGFX_DIR .. "examples/common/**.mm",
+		}
+		links {
+			"Cocoa.framework",
+			"OpenGL.framework",
+		}
+
+	configuration { "xcode4" }
+		platforms {
+			"Universal"
+		}
+		files {
+			BGFX_DIR .. "examples/common/**.mm",
+		}
+		links {
+			"Cocoa.framework",
+			"Foundation.framework",
+			"OpenGL.framework",
+		}
+
+	configuration { "ios*" }
+		kind "ConsoleApp"
+		files {
+			BGFX_DIR .. "examples/common/**.mm",
+		}
+		linkoptions {
+			"-framework CoreFoundation",
+			"-framework Foundation",
+			"-framework OpenGLES",
+			"-framework UIKit",
+			"-framework QuartzCore",
+		}
+
+	configuration { "qnx*" }
+		targetextension ""
+		links {
+			"EGL",
+			"GLESv2",
+		}
+
+	configuration {}
+
+	strip()
+end
---
+++ src/luminos.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2011-2014 Branimir Karadzic. All rights reserved.
+ * License: http://www.opensource.org/licenses/BSD-2-Clause
+ */
+
+#include "common.h"
+#include <bgfx.h>
+
+#include "SDL.h"
+#undef main
+#include "SDL_syswm.h"
+
+int _main_(int /*_argc*/, char** /*_argv*/)
+{
+	uint32_t width = 1280;
+	uint32_t height = 720;
+	uint32_t debug = BGFX_DEBUG_TEXT;
+	uint32_t reset = BGFX_RESET_VSYNC;
+
+	bgfx::init();
+	bgfx::reset(width, height, reset);
+
+	// Enable debug text.
+	bgfx::setDebug(debug);
+
+	// Set view 0 clear state.
+	bgfx::setViewClear(0
+		, BGFX_CLEAR_COLOR_BIT|BGFX_CLEAR_DEPTH_BIT
+		, 0x303030ff
+		, 1.0f
+		, 0
+		);
+
+	while (!entry::processEvents(width, height, debug, reset) )
+	{
+		// Set view 0 default viewport.
+		bgfx::setViewRect(0, 0, 0, width, height);
+
+		// This dummy draw call is here to make sure that view 0 is cleared
+		// if no other draw calls are submitted to view 0.
+		bgfx::submit(0);
+
+		// Use debug font to print information about this example.
+		bgfx::dbgTextClear();
+		bgfx::dbgTextPrintf(0, 1, 0x4f, "bgfx/examples/00-helloworld");
+		bgfx::dbgTextPrintf(0, 2, 0x6f, "Description: Initialization and debug text.");
+
+		// Advance to next frame. Rendering thread will be kicked to 
+		// process submitted rendering primitives.
+		bgfx::frame();
+	}
+
+	// Shutdown bgfx.
+	bgfx::shutdown();
+
+	return 0;
+}
